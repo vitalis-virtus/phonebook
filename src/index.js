aaa71b9dde569d73e416aac78421f6b4a2a59568
vitalis-virtus <68068673+vitalis-virtus@users.noreply.github.com>
2022-06-04 11:37:28 +0300

    add sass structure and new favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import 'normalize.css';
+// import "./styles.css";
+import './styles/index.scss'
 import App from "./App.js";
 import store from "./redux/store";
 import { Provider } from "react-redux";
